188bdd46aa46f4f62047f435d7129ab43558a6ec
Maheesha Adelina <MaheeshaAdelina@gmail.com>
2023-12-02 18:33:14 +0530

    day 1 part 2 maybe works

--- aoc_d1.exs
+++ aoc_d1.exs
@@ -10,6 +10,7 @@ zoneight234
 7pqrstsixteen
 """
 
+numList = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 inputList = String.split input, "\n"
 
 defmodule ModuleStuff do
@@ -26,14 +27,45 @@ defmodule ModuleStuff do
     )
   end
 
+  def tortureToExtractTheNum(string, matcherList) do
+    containedNums = Enum.filter(matcherList, fn x -> String.contains?(string, x) end)
+    stringToNumList = %{"one"=>"1", "two"=>"2", "three"=>"3", "four"=>"4", "five"=>"5", "six"=>"6", "seven"=>"7", "eight"=>"8", "nine"=>"9"}
+
+    recurseAndGetNumsInOrder(string, containedNums, []) |> Enum.reverse |> Enum.reduce("", fn numStr, acc -> acc <> Map.fetch!(stringToNumList, numStr) end)
+
+  end
+
+  def recurseAndGetNumsInOrder(string, matcherList, currentColl) do
+    unless Enum.empty?(matcherList) do
+      startingNum = Enum.find(matcherList, fn x -> String.starts_with?(string, x) end)
+      unless startingNum == nil do
+        recurseAndGetNumsInOrder(String.slice(string, 1, 100), Enum.reject(matcherList, fn x -> x == startingNum end), [startingNum | currentColl])
+      else
+        recurseAndGetNumsInOrder(String.slice(string, 1,100), matcherList, currentColl)
+      end
+    else
+      currentColl
+    end
+  end
+
 end
 
 outputList = Enum.map(inputList,
   fn string ->
-    mixedList = String.split(string, ~r{[0-9]+}, trim: true, include_captures: true)
-    Enum.m
+    String.split(string, ~r{[0-9]+}, trim: true, include_captures: true) |> Enum.map(
+      fn listItem ->
+        unless String.match?(listItem, ~r{[0-9]+}) do
+          if String.contains?(listItem, numList) do
+            ModuleStuff.tortureToExtractTheNum(listItem, numList)
+          else
+            ""
+          end
+        else
+          listItem
+        end
+      end
+    ) |> Enum.reject(fn x -> x == "" end) |> List.to_string
   end
-)
+) |> List.delete("") |> Enum.map(fn x -> String.codepoints(x) |> ModuleStuff.doStuff end)
 
-
-#IO.puts(ModuleStuff.addListTogether(tl Enum.reverse(outputList)))
+IO.puts(ModuleStuff.addListTogether(outputList))
